a9d955eb40adc56af7ad9236f0343b5a8dbb6e3c
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2023-02-23 19:36:46 -0500

    Reformatted post summary item (turned into d-flex)

--- src/tuiter/post-summary-list/post-summary-item.js
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -12,13 +12,13 @@ const PostSummaryItem = (
 ) => {
     return(
         <li className="list-group-item">
-            <div className="row">
-                <div className="col-10">
-                    <div>{post.userName} . {post.time}</div>
+            <div className="d-flex justify-content-between">
+                <div>
+                    <div>{post.userName} &#183; {post.time}</div>
                     <div className="fw-bolder">{post.topic}</div>
                     <div>{post.title}</div>
                 </div>
-                <div className="col-2">
+                <div className="ps-2">
                     <img width={70} className="float-end rounded-3" src={`/images/${post.image}`}/>
                 </div>
             </div>
